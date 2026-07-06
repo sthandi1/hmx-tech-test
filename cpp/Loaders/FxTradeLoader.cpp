329b5401262899a6f762446795a0c81e40949b1a
Samvir Thandi <sthandi@me.com>
2026-07-06 20:54:27 +0100

    Add framework for reading from line

--- cpp/Loaders/FxTradeLoader.cpp
+++ cpp/Loaders/FxTradeLoader.cpp
@@ -1,7 +1,22 @@
 #include "FxTradeLoader.h"
+
+#include <sstream>
 #include <stdexcept>
+#include <memory>
+#include <vector>
+#include <string>
 
 // NOTE: These methods are only here to allow the solution to compile prior to the test being completed.
+
+std::unique_ptr<FxTrade> FxTradeLoader::createTradeFromLine(const std::string& line) {
+
+    std::vector<std::string> items;
+    std::stringstream ss(line);
+    std::string item;
+
+
+    return {};
+}
 
 std::vector<ITrade*> FxTradeLoader::loadTrades() {
     throw std::runtime_error("Not implemented");
--- cpp/Loaders/FxTradeLoader.h
+++ cpp/Loaders/FxTradeLoader.h
@@ -7,14 +7,18 @@
 #include <vector>
 
 class FxTradeLoader : public ITradeLoader {
-private:
+    static std::unique_ptr<FxTrade> createTradeFromLine(const std::string &line);
+
     std::string dataFile_;
-    
+    const std::string separator = "\xC2\xAC";
+
 public:
     // NOTE: These methods are only here to allow the solution to compile prior to the test being completed.
-    std::vector<ITrade*> loadTrades() override;
-    std::string getDataFile() const override;
-    void setDataFile(const std::string& file) override;
+    std::vector<ITrade *> loadTrades() override;
+
+    [[nodiscard]] std::string getDataFile() const override;
+
+    void setDataFile(const std::string &file) override;
 };
 
 #endif // FXTRADELOADER_H
