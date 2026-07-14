9d51b11363719dacde2083761968f3f46a44b050
Samvir Thandi <sthandi@me.com>
2026-07-14 20:46:44 +0100

    Implement screen result printer

--- cpp/RiskSystem/ScreenResultPrinter.cpp
+++ cpp/RiskSystem/ScreenResultPrinter.cpp
@@ -1,8 +1,37 @@
 #include "ScreenResultPrinter.h"
 #include <iostream>
 
-void ScreenResultPrinter::printResults(ScalarResults& results) {
-    for (const auto& result : results) {
+void ScreenResultPrinter::printResults(ScalarResults &results) {
+    for (const auto &result: results) {
+        const std::string tradeId = result.getTradeId();
+        const std::optional<double> resultValue = result.getResult();
+        std::optional<std::string> errorValue = result.getError();
+
+        std::string line;
+        if (resultValue && errorValue) {
+            std::cout << "BOTH" << "\n";
+            line.append(tradeId);
+            line.append(" : ");
+            line.append(std::to_string(resultValue.value()));
+            line.append(" : ");
+            line.append(errorValue.value());
+            line.append("\n");
+            std::cout << line;
+        } else if (resultValue) {
+            line.append(tradeId);
+            line.append(" : ");
+            line.append(std::to_string(resultValue.value()));
+            line.append("\n");
+            std::cout << line;
+        } else if (errorValue) {
+            line.append(tradeId);
+            line.append(" : ");
+            line.append(errorValue.value());
+            line.append("\n");
+            std::cout << line;
+        } else {
+            std::cout << "Unable to print tradeID: " << tradeId << "\n";
+        }
         // Write code here to print out the results such that we have:
         // TradeID : Result : Error
         // If there is no result then the output should be:
